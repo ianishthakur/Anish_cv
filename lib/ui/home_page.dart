5159b69366138439952a68dbc4ce8b0917a6c2c4
ianishthakur <1anishthakur@gmail.com>
2023-03-19 16:42:40 +0545

    app icon changed

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -7,27 +7,32 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Helloo")),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Container(
-                width: 100,
-                height: 100,
-                decoration: BoxDecoration(
-                    // color: Colors.white,
-                    image: DecorationImage(
-                      image: Image.asset(Assets.profile).image,
-                      fit: BoxFit.cover,
-                    ),
-                    border: Border.all(width: 1, color: Colors.grey.shade300),
-                    borderRadius: BorderRadius.circular(15)),
-              ),
-            ],
-          )
-        ],
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(title: Text("Helloo")),
+        body: Column(
+          children: [
+            Row(
+              children: [
+                Center(
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                        // color: Colors.white,
+                        image: DecorationImage(
+                          image: Image.asset(Assets.profile).image,
+                          fit: BoxFit.cover,
+                        ),
+                        border:
+                            Border.all(width: 1, color: Colors.grey.shade300),
+                        borderRadius: BorderRadius.circular(15)),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
